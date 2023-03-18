a895bf29785bee66f9aa9e3b249371ffbeedbce7
charliebee37 <charliesamie@yahoo.com>
2023-03-18 03:43:47 +0100

    task sixsix

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,13 +3,13 @@
 /**
  * main - main function
  *
- * Return: Always 0 (Success)
+ * Return: always 0
  */
 int main(void)
 {
 	int i;
 
-	for (i = 0 ; i < 10 ` i++)
+	for (i = 0 ; i < 10 ; i++)
 		putchar(i + '0');
 	putchar('\n');
 	return (0);
